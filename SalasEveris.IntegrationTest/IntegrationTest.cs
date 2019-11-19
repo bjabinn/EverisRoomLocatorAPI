190ab484ae256f8f36888967bf5bebd44271462a
Jose Ant. Beltran <bjabinn@hotmail.com>
2019-11-19 23:40:28 +0100

    Adding integrationTests

---
+++ SalasEveris.IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,17 @@
+using System.Net.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SalasEveris;
+
+namespace SalasEveris.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            TestClient = appFactory.CreateClient();
+        }        
+    }
+}
